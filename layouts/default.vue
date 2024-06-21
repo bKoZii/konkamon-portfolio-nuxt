f46cefbe31405df63466b65af8fd1d98b65fe9fe
Konkamon Sion <bigkonkamon@gmail.com>
2024-06-21 16:00:27 +0700

    feat: :lipstick: Updated Background Color
    Updated Background Color to `slate-200` fot better contrast

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="inset-0 h-full min-h-dvh w-full bg-white dark:bg-neutral-950 bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:42px_42px]"
+    class="inset-0 h-full min-h-dvh w-full bg-slate-200 dark:bg-neutral-950 bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:42px_42px]"
   >
     <!-- <div class="dark:bg-neutral-900 min-h-dvh"> -->
     <div class="container mx-auto max-w-[800px] py-5">
