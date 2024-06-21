449241de983461c1655c628e863d6bb35e4512ff
Konkamon Sion <bigkonkamon@gmail.com>
2024-06-21 14:02:21 +0700

    feat(Bookmarks): :sparkles: Initial Release
    Created Bookmark Page - List of recommended website.

---
+++ components/Bookmark/Item.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <a
+      :href="webItem.url"
+      target="_blank"
+      ref="noopener noreferrer"
+      class="flex items-center gap-3 transition-all p-2 rounded-lg min-w-0 active:scale-[0.98]"
+    >
+      <span class="relative inline-flex items-center justify-center flex-shrink-0 text-gray-600 dark:text-gray-400 rounded-md h-8 w-8"
+        ><Icon name="ph:globe-duotone" mode="svg" size="30px"
+      /></span>
+      <div class="flex flex-col truncate hover:text-primary">
+        <p class="truncate text-black dark:text-white font-semibold">{{ webItem.name }}</p>
+        <p class="truncate text-xs text-gray-400">{{ webItem.description }}</p>
+      </div>
+      <span class="flex-1"></span><span class="text-sm text-gray-400 dark:text-gray-500">{{ getHostname(webItem.url) }}</span>
+    </a>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type BookmarkItemsInterface from '../../types/BookmarkItemsInterface'
+
+defineProps({
+  webItem: {
+    type: Object as () => BookmarkItemsInterface,
+    required: true
+  }
+})
+
+const getHostname = (url: string) => {
+  const hostname = new URL(url).hostname
+  return hostname.replace(/^www\./, '')
+}
+</script>
+
+<style></style>
---
+++ pages/Bookmarks.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <article class="prose dark:prose-invert">
+      <h2>Bookmarks</h2>
+      <p>รวมเว็บไซต์แนว IT, Linux, Tutorials และเว็บไซต์ที่อื่นๆ ที่น่าสนใจ</p>
+    </article>
+    <div class="flex flex-col flex-nowrap gap-3 mt-6">
+      <div
+        v-for="data in bookmarkItem"
+        :key="data.name"
+        class="hover:bg-gray-200 dark:hover:bg-neutral-800 rounded-lg hover:shadow-md transition-all"
+      >
+        <BookmarkItem :web-item="data" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type BookmarkItemsInterface from '~/types/BookmarkItemsInterface'
+
+const bookmarkItem: BookmarkItemsInterface[] = [
+  {
+    name: 'IT Tools - Handy Tools for Developers',
+    url: 'https://it-tools.tech',
+    description: 'เว็บไซต์รวมเครื่องมือต่างๆ สำหรับชาว IT ไว้ประมาณ 80 เครื่องมือ'
+  },
+  {
+    name: 'Transform - a Web Coverter',
+    url: 'https://transform.tools',
+    description: 'รวมเครื่องมือแปลง Code ต่างๆ สำหรับสาย Coding'
+  },
+  {
+    name: 'HTML Emmet Cheat Sheet',
+    url: 'https://docs.emmet.io/cheat-sheet',
+    description: 'Cheat Sheet สำหรับ HTML Emmet ช่วยให้พิมพ์ HTML ได้เร็วขึ้น'
+  },
+  {
+    name: 'BezKoder - Mobile & Web App Development Tutorial',
+    url: 'https://www.bezkoder.com',
+    description: 'รวม Tutorial การพัฒนา Mobile App และ Web App ด้วย Framework ต่างๆ'
+  },
+  {
+    name: '60 Essential Linux Commands + Cheat Sheet',
+    url: 'https://www.hostinger.com/tutorials/linux-commands',
+    description: 'รวม Linux Command ที่ใช้บ่อยที่สุด พร้อม Cheat Sheet'
+  },
+  {
+    name: 'Finisher - Free Animated Headers ',
+    url: 'https://www.finisher.co/lab/header',
+    description: 'สร้าง Animated Header ได้ง่ายๆ ปรับแต่งได้หลากหลาย'
+  },
+  {
+    name: 'Developer Roadmaps',
+    url: 'https://roadmap.sh',
+    description: 'รวม Roadmap สายงาน Developer ต่างๆ'
+  },
+  {
+    name: 'Color Hunt - Color Palettes for Designers and Artists',
+    url: 'https://colorhunt.co',
+    description: 'รวม Color Palettes ไว้มากมาย หลากหลายโทนสี'
+  },
+  {
+    name: 'Monkeytype',
+    url: 'https://monkeytype.com/',
+    description: 'วัดความเร็วในการพิมพ์ มีลูกเล่นมากมาย Customize ได้หลากหลาย'
+  }
+]
+</script>
+
+<style></style>
