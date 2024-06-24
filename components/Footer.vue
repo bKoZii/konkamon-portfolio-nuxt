9c6b3c09b8c5e339f583dfe852f26f63c15d008e
Konkamon Sion <bigkonkamon@gmail.com>
2024-06-24 19:05:15 +0700

    feat: :lipstick: Updated Line Break
    Break the line between my name and Open Source

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,9 +2,12 @@
   <footer
     class="text-gray-500 bg-white drop-shadow-md dark:drop-shadow-none flex flex-col flex-nowrap items-center gap-5 py-4 dark:bg-neutral-900 border dark:border-neutral-800 rounded-lg"
   >
-    <h1>© 2024 กรกมล ศรีอ่อน - ไม่มีลิขสิทธิ์ เพราะเป็น Open Source!</h1>
+    <div class="flex flex-col items-center">
+      <span>© 2024 กรกมล ศรีอ่อน</span>
+      <span>ไม่มีลิขสิทธิ์ เพราะเป็น Open Source!</span>
+    </div>
     <a href="https://github.com/bKoZii/konkamon-portfolio-nuxt" target="_blank" ref="noopener noreferrer">
-      <div class="hover:text-black">
+      <div class="active:scale-95 hover:text-black dark:hover:text-white">
         <Icon name="ph:github-logo-duotone" size="28px" mode="svg" />
       </div>
     </a>
