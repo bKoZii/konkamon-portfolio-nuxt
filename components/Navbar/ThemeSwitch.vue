8d6880366105c71f8ac9f09a37d5ba5cf1396247
Konkamon Sion <bigkonkamon@gmail.com>
2024-06-24 20:19:14 +0700

    feat: :sparkles: ClientOnly Theme Button
    Make Theme Switch Button only on Client Side

--- components/Navbar/ThemeSwitch.vue
+++ components/Navbar/ThemeSwitch.vue
@@ -1,10 +1,23 @@
 <template>
   <div>
-    <button class="inline-flex text-gray-400 group relative hover:text-gray-900 dark:hover:text-gray-100" @click="isDark = !isDark">
+    <!-- <button class="inline-flex text-gray-400 group relative hover:text-gray-900 dark:hover:text-gray-100" @click="isDark = !isDark">
       <UTooltip text="Toggle Theme" :popper="{ strategy: 'absolute' }">
-        <Icon :name="colorMode.value == 'dark' ? 'ph:cloud-sun-duotone' : 'ph:moon-stars-duotone'" size="24px" />
-      </UTooltip>
-    </button>
+        <Icon :name="isDark ? 'ph:cloud-sun-duotone' : 'ph:moon-stars-duotone'" size="24px" />
+      </UTooltip> -->
+    <ClientOnly>
+      <UButton
+        :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'"
+        color="gray"
+        variant="ghost"
+        aria-label="Theme"
+        :padded="false"
+        @click="isDark = !isDark"
+      />
+      <template #fallback>
+        <USkeleton class="h-5 w-5" :ui="{ rounded: 'rounded-full' }" />
+      </template>
+    </ClientOnly>
+    <!-- </button> -->
   </div>
 </template>
 
@@ -17,6 +30,10 @@ const isDark = computed({
   set() {
     colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
   }
+})
+
+onMounted(() => {
+  console.log(isDark)
 })
 </script>
 
