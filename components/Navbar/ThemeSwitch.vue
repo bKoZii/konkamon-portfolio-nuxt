735a35fcfa3c6a7e3996f8d1362fae248996dd2a
Konkamon Sion <bigkonkamon@gmail.com>
2024-06-25 11:26:05 +0700

    fix(ThemeSwitch): :pencil2: Missing Popper's `absolute` strategy config
    Added missing Popper's `absolute` strategy config for the tooltip

--- components/Navbar/ThemeSwitch.vue
+++ components/Navbar/ThemeSwitch.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="inline-flex">
     <ClientOnly>
-      <UTooltip :text="tooltipText">
+      <UTooltip :text="tooltipText" :popper="{ strategy: 'absolute' }">
         <button class="inline-flex text-gray-400 group relative hover:text-gray-900 dark:hover:text-gray-100" @click="swtichTheme()">
           <Icon :name="isDark ? 'ph:cloud-sun-duotone' : 'ph:moon-stars-duotone'" size="24px" />
         </button>
