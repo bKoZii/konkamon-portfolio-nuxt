e97f4aab2c087e91a9ea29235349c2d31805099c
Konkamon Sion <bigkonkamon@gmail.com>
2024-06-25 10:37:50 +0700

    feat: :lipstick: Improved 3-Sections Gap
    Improved Gap between Nav, Container, Footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,19 @@
 <template>
   <div
-    class="inset-0 h-full min-h-dvh w-full bg-slate-200 dark:bg-neutral-950 bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:42px_42px]"
-  >
-    <!-- <div class="dark:bg-neutral-900 min-h-dvh"> -->
-    <div class="container mx-auto max-w-[800px] py-5">
-      <Navbar />
-      <main class="my-8 bg-white dark:bg-neutral-900 p-8 border rounded-lg dark:border-neutral-800 drop-shadow-md dark:drop-shadow-none">
-        <slot />
-      </main>
-      <Footer />
-    </div>
-  </div>
+    class="fixed inset-0 h-full w-full bg-slate-200 dark:bg-neutral-950 bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:42px_42px]"
+  ></div>
+
+  <!-- <div class="dark:bg-neutral-900 min-h-dvh"> -->
+  <!-- <div class="container mx-auto max-w-[800px] py-5"> -->
+  <UContainer class="max-w-[850px] py-5 flex flex-col gap-5 flex-nowrap">
+    <Navbar />
+    <main class="bg-white dark:bg-neutral-900 p-3 lg:p-8 border rounded-lg dark:border-neutral-800 drop-shadow-md dark:drop-shadow-none">
+      <slot />
+    </main>
+    <Footer />
+  </UContainer>
+  <!-- </div> -->
+  <!-- </div> -->
 </template>
 
 <script lang="ts" setup></script>
