37264f82b2bf7bd917627ec76e8f9da5f4900bcb
Konkamon Sion <bigkonkamon@gmail.com>
2024-06-26 21:58:51 +0700

    feat: :lipstick: Darken Icon Color / Sticky Navbar
    Darken Navbar Icon Color / Navbar is now sticky to the top

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="sticky container mx-auto max-w-[1000px] rounded-lg px-6 py-2 shadow-md z-10 bg-white bg-opacity-80 backdrop-blur-lg border dark:bg-neutral-900 dark:text-white dark:border-neutral-800"
+    class="sticky top-0 rounded-lg px-6 py-2 shadow-md z-10 bg-white bg-opacity-80 backdrop-blur-lg border dark:bg-neutral-900 dark:text-white dark:border-neutral-800"
   >
     <header class="flex items-center justify-between sm:justify-between xs:justify-center">
       <ULink id="myName" to="/" class="hidden sm:inline-flex items-center gap-2.5 text-xl font-extrabold" aria-label="logo"> KONKAMON </ULink>
@@ -10,7 +10,7 @@
             <ULink
               :to="data.link"
               active-class="text-primary"
-              class="group relative inline-flex text-gray-400 transition duration-100 hover:text-gray-800 dark:hover:text-white"
+              class="inline-flex text-gray-600 transition duration-100 hover:text-gray-900 dark:hover:text-white"
             >
               <icon :name="data.icon" size="24px" mode="svg" class="active:scale-90" />
             </ULink>
