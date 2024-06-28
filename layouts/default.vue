9de1f146432f50b8c73018cae81eb91f5447802a
Konkamon Sion <bigkonkamon@gmail.com>
2024-06-28 18:46:00 +0700

    feat: :lipstick: Full screen width Navbar
    Navbar is now display in Full screen Width

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,8 +3,8 @@
     class="fixed inset-0 h-full w-full bg-slate-200 bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:42px_42px] dark:bg-neutral-950"
   ></div>
 
+  <Navbar />
   <UContainer class="flex max-w-[850px] flex-col flex-nowrap gap-5 py-5">
-    <Navbar />
     <main class="rounded-lg border bg-white p-3 drop-shadow-md lg:p-8 dark:border-neutral-800 dark:bg-neutral-900 dark:drop-shadow-none">
       <slot />
     </main>
