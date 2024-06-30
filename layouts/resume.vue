e90e53f492393649cb8f14e9cadfd1170aa84fae
Konkamon Sion <bigkonkamon@gmail.com>
2024-07-01 00:36:36 +0700

    feat(Resume): :sparkles: Layout for Resume Page
    Created a dedicated layout for resume page

---
+++ layouts/resume.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <Navbar class="print:hidden" />
+    <div class="bg-primary-600 dark:bg-primary-800 p-2 text-center text-white print:hidden">
+      <p>
+        Printer-friendly standard résumé, any HTML tags with
+        <code class="rounded bg-neutral-900 p-1 text-sm">print:hidden</code> class will be hidden on print. Press Ctrl+P to print
+      </p>
+    </div>
+    <div class="container mx-auto max-w-screen-xl xl:mx-auto">
+      <slot />
+    </div>
+  </main>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
