923babc5e552225b9e64b45d90ce9014d7c8cc31
Konkamon Sion <bigkonkamon@gmail.com>
2024-07-06 22:44:43 +0700

    feat(Blog): :lipstick: Updated Card / Added `introText`
    Updated Card Style / Added `introText` below the title

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -5,20 +5,23 @@
         <h2>My Blog</h2>
         <p>รวม Blog ต่างๆ ด้าน IT / โดย Sanity CMS</p>
       </article>
-      <section class="grid grid-cols-1 gap-3 sm:grid-cols-1 md:grid-cols-2">
+      <section class="grid grid-cols-1 gap-x-3 gap-y-5 sm:grid-cols-1 md:grid-cols-2">
         <div v-for="(post, index) in data" :key="index">
           <ULink :to="`/blog/${post.slug.current}`">
             <UCard
               :ui="{
+                base: 'hover:scale-[1.01] transition-all hover:drop-shadow-md',
+                background: 'dark:bg-neutral-800',
                 body: {
                   base: 'hover:text-primary prose-h2:hover:text-primary',
                   padding: 'p-0 sm:px-0 sm:pt-0 md:p-0'
                 }
               }"
             >
-              <SanityImage :asset-id="post.mainImage?.asset._ref" class="rounded-t-lg" />
+              <SanityImage v-if="post.mainImage" :asset-id="post.mainImage?.asset._ref" class="rounded-t-lg" />
               <div class="prose prose-sm dark:prose-invert sm:prose-sm md:prose-md prose-h2:m-0 flex max-w-none flex-col gap-2 p-5">
                 <h2>{{ post.title }}</h2>
+                <p class="text-xs">{{ post.introText }}</p>
                 <section class="flex flex-row gap-1" v-if="post.categories">
                   <div v-for="(category, index) in post.categories" :key="index">
                     <UBadge size="xs">{{ category.title }}</UBadge>
@@ -45,6 +48,7 @@ import type { IBlogIndex } from '../../types/BlogIndexInterface'
 const query = groq`*[_type == "post"] {
 title,
   author->{name},
+  introText,
   mainImage {
     ...,
     asset{_ref}
