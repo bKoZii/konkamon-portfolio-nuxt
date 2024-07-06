e3d1ca2c211a8c40521e2efccdaaf3aff78a6b2a
Konkamon Sion <bigkonkamon@gmail.com>
2024-07-06 15:45:13 +0700

    feat(Blog): :construction: Image Display for `SanityContent` (WIP)

---
+++ components/Blog/Image.vue
@@ -0,0 +1,19 @@
+<template>
+  <SanityImage class="rounded-lg shadow w-full" :asset-id="asset._ref" auto="format" fit="fillmax" />
+</template>
+
+<script setup>
+// import { ref, computed } from 'vue';
+// import imageUrlBuilder from '@sanity/image-url';
+
+const props = defineProps({
+  asset: {
+    type: Object,
+    required: true
+  }
+})
+
+// const { config } = useSanity();
+// const builder = imageUrlBuilder(config);
+// const url = computed(() => builder.image(props.asset._ref).url());
+</script>
