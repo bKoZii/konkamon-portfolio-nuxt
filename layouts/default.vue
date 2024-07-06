42dded150cf3514d62abc490c3d0ee64075d7e49
Konkamon Sion <bigkonkamon@gmail.com>
2024-07-06 23:35:18 +0700

    feat: :lipstick: Removed Padding on Small Screen

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <div
-      class="fixed inset-0 -z-10 h-full w-full bg-slate-200 bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:42px_42px] dark:bg-neutral-950"
+      class="hidden sm:block fixed inset-0 -z-10 h-full w-full bg-slate-200 bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:42px_42px] dark:bg-neutral-950"
     ></div>
     <Navbar />
-    <UContainer class="flex max-w-[850px] flex-col flex-nowrap gap-5 py-5">
+    <UContainer class="flex max-w-[950px] flex-col flex-nowrap gap-0 sm:gap-5" :ui="{padding:'p-0 xs:p-0 sm:p-3'}">
       <main class="rounded-lg border bg-white p-3 drop-shadow-md lg:p-8 dark:border-neutral-800 dark:bg-neutral-900 dark:drop-shadow-none">
         <slot />
       </main>
