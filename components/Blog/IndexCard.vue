10f9e199aaadcf8f9dcd779f5e9a5a924ec7eb20
Konkamon Sion <bigkonkamon@gmail.com>
2024-07-07 17:19:07 +0700

    feat(Blog): :sparkles: BlogIndexCard Component
    Added Blog Index Page's Card Component

---
+++ components/Blog/IndexCard.vue
@@ -0,0 +1,46 @@
+<template>
+  <template v-if="post">
+    <ULink :to="`/blog/${post.slug.current}`">
+      <UCard
+        :ui="{
+          base: 'hover:scale-[1.01] transition-all hover:drop-shadow-md',
+          background: 'dark:bg-neutral-800',
+          body: {
+            base: 'hover:text-primary prose-h2:hover:text-primary',
+            padding: 'p-0 sm:px-0 sm:pt-0 md:p-0'
+          }
+        }"
+      >
+        <SanityImage v-if="post.mainImage" :asset-id="post.mainImage?.asset._ref" class="rounded-t-lg" />
+        <div class="prose prose-sm dark:prose-invert sm:prose-sm md:prose-md prose-h2:m-0 prose-p:m-0 flex max-w-none flex-col gap-2 p-5">
+          <div class="not-prose flex flex-row items-center space-x-2">
+            <UAvatar :src="post.author.image.asset.url" alt="Avatar" size="xs" />
+            <p class="m-0 text-xs">{{ `${post.author.name}` }}</p>
+          </div>
+          <h2>{{ post.title }}</h2>
+          <p class="text-xs">{{ post.introText }}</p>
+          <section class="not-prose flex flex-row gap-1" v-if="post.categories">
+            <div v-for="(category, index) in post.categories" :key="index">
+              <UBadge size="xs" color="primary" variant="soft">{{ category.title }}</UBadge>
+            </div>
+          </section>
+          <div class="not-prose flex flex-row items-center gap-1">
+            <UIcon name="ph:calendar-dots-duotone" class="size-5" />
+            <UTooltip :text="useFormatDate(post._createdAt)" :popper="{ arrow: true, strategy: 'absolute', offsetDistance: 0 }">
+              <p class="m-0 text-xs">{{ `${useRelativeDate(post._createdAt)}` }}</p>
+            </UTooltip>
+          </div>
+        </div>
+      </UCard>
+    </ULink>
+  </template>
+</template>
+
+<script lang="ts" setup>
+import type { Root2 } from '~/types/BlogIndexInterface'
+import { defineProps } from 'vue'
+const prop = defineProps<{ post: Root2 }>()
+const { post } = toRefs(prop)
+</script>
+
+<style></style>
