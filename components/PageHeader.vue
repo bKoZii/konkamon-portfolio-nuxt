7b2dcdc3c7b5478516ddf6ccba00f061c514b844
Konkamon Sion <bigkonkamon@gmail.com>
2024-07-07 16:36:39 +0700

    feat(Pages): :sparkles: Page Header Component

---
+++ components/PageHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <article class="prose prose-sm md:prose-base dark:prose-invert max-w-none text-center lg:text-left prose-h1:m-0">
+      <h1>{{ data.title }}</h1>
+      <p>{{ data.description }}</p>
+    </article>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const pageHeader = defineProps({
+  title: String,
+  description: String
+})
+const data = markRaw(pageHeader)
+</script>
+
+<style></style>
