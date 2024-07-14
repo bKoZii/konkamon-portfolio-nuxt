db2ef4365d2831551709b11d66b771285c265a21
Konkamon Sion <bigkonkamon@gmail.com>
2024-07-14 19:22:36 +0700

    feat(Footer): :lipstick: `flex-row` footer with `flex-col` on sm Screen

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,8 +1,8 @@
 <template>
   <footer
-    class="flex flex-col flex-nowrap items-center gap-5 rounded-none sm:rounded-lg border bg-white py-4 text-gray-500 drop-shadow-md dark:border-neutral-800 dark:bg-neutral-900 dark:drop-shadow-none"
+    class="flex flex-col flex-nowrap items-center justify-between gap-5 rounded-none border bg-white px-8 py-4 text-gray-500 drop-shadow-md sm:flex-row sm:rounded-lg dark:border-neutral-800 dark:bg-neutral-900 dark:drop-shadow-none"
   >
-    <div class="flex flex-col items-center">
+    <div class="flex flex-col items-center text-sm sm:items-start">
       <span>© 2024 กรกมล ศรีอ่อน</span>
       <span>ไม่มีลิขสิทธิ์ เพราะเป็น Open Source!</span>
     </div>
