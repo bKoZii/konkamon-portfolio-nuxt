d655d2a3d3054dc653e3f4ba3d6260f2a0abb798
Konkamon Sion <bigkonkamon@gmail.com>
2024-07-15 08:54:18 +0700

    feat(Blog): :sparkles: Skeleton for Blog Card Fallback
    Added Blog Cards fallback component

---
+++ components/Blog/SkeletonFallback.vue
@@ -0,0 +1,14 @@
+<template>
+  <UCard class="h-fit">
+    <div class="flex flex-col space-y-4">
+      <USkeleton class="h-40 w-full" />
+      <USkeleton class="h-4 w-full" />
+      <USkeleton class="h-10 w-full" />
+      <USkeleton class="h-6 w-full" />
+    </div>
+  </UCard>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
