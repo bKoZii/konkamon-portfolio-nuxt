585783c28d2709262debdd710585b7782ebf8133
Konkamon Sion <bigkonkamon@gmail.com>
2024-07-18 16:00:04 +0700

    feat(Technology): :lipstick: Using `UBadge` to display Technologies
    Using Badge with Icon instead of Only Icon with Tooltip

--- components/Technology/GridItems.vue
+++ components/Technology/GridItems.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
     <div class="flex w-full flex-col dark:text-gray-200">
-      <h3 class="font-bold">{{ title }}</h3>
-      <div class="flex flex-row justify-center gap-1 sm:justify-start">
+      <h3 class="mb-2 font-bold">{{ title }}</h3>
+      <div class="flex flex-row flex-wrap justify-center gap-1 sm:justify-start">
         <div v-for="item in technologyData" :key="item.name">
-          <UTooltip :text="item.name" :popper="{ arrow: true, strategy: 'absolute' }">
-            <UIcon :name="item.icon" dynamic size="36px" />
-          </UTooltip>
+          <UBadge class="flex flex-row gap-1" variant="subtle" size="xs">
+            <UIcon :name="item.icon" class="size-6" />
+            {{ item.name }}
+          </UBadge>
         </div>
       </div>
     </div>
