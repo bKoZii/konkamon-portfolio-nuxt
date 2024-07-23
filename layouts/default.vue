e846b8be81dd4367b6bfa5c31d26acf702c5dcb4
Konkamon Sion <bigkonkamon@gmail.com>
2024-07-23 09:02:23 +0700

    feat: :lipstick: Added Gradient Background
    Added Gradient Element at background

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,9 +3,16 @@
     <div
       class="fixed inset-0 -z-10 h-full w-full bg-slate-200 bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:42px_42px] dark:bg-neutral-950"
     ></div>
+    <div class="fixed inset-0 -z-[9] mx-28 hidden h-screen items-center justify-center lg:flex">
+      <div
+        class="from-primary via-background h-[500px] w-full rounded-full bg-gradient-to-r to-blue-500 opacity-20 blur-[150px] dark:to-blue-600 dark:opacity-15"
+      ></div>
+    </div>
     <Navbar />
-    <UContainer class="flex max-w-[950px] flex-col flex-nowrap gap-2 sm:gap-5" :ui="{padding:'p-0 xs:p-0 sm:p-3'}">
-      <main class="rounded-none sm:rounded-lg border bg-white p-3 xs:p-5 md:p-5 drop-shadow-md lg:p-8 dark:border-neutral-800 dark:bg-neutral-900 dark:drop-shadow-none">
+    <UContainer class="flex max-w-[950px] flex-col flex-nowrap gap-2 sm:gap-5" :ui="{ padding: 'p-0 xs:p-0 sm:p-3' }">
+      <main
+        class="rounded-none border bg-white p-3 drop-shadow-md xs:p-5 sm:rounded-lg md:p-5 lg:p-8 dark:border-neutral-800 dark:bg-neutral-900 dark:drop-shadow-none"
+      >
         <slot />
       </main>
       <Footer />
