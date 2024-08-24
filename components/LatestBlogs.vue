dd12e16b1783468fbc82ae2801fa866a18ee4f8b
Konkamon Sion <bigkonkamon@gmail.com>
2024-08-24 19:49:29 +0700

    feat(latestBlogs): :zap: use 'useAsyncData' for fetching

--- components/LatestBlogs.vue
+++ components/LatestBlogs.vue
@@ -6,8 +6,8 @@
     </section>
     <section class="grid grid-cols-1 gap-3 sm:grid-cols-1 md:grid-cols-2">
       <ClientOnly>
-        <div v-for="(post, index) in latestBlogs" :key="index">
-          <BlogIndexCard :post="post" />
+        <div v-for="post in latestBlogs?.data" :key="post.id">
+          <BlogIndexCard :post="post.attributes" />
         </div>
         <template #fallback>
           <div v-for="fallback in 2" :key="fallback">
@@ -20,44 +20,29 @@
 </template>
 
 <script lang="ts" setup>
-import type { Strapi4RequestParams } from '@nuxtjs/strapi';
-import type { StrapiBlogs } from '~/types/StrapiBlogs';
+import type { Strapi4RequestParams } from '@nuxtjs/strapi'
+import type { StrapiBlogs } from '~/types/StrapiBlogs'
 
 const { find } = useStrapi()
-const latestBlogs = ref<StrapiBlogs[]>()
-
-const fetchBlogs = async () => {
-  const params: Strapi4RequestParams = {
-    fields: ['title', 'subtitle', 'publishedAt', 'slug'],
-    sort: 'publishedAt:desc',
-    pagination: {
-      pageSize: 4,
-      page: 1
+const params: Strapi4RequestParams = {
+  fields: ['title', 'subtitle', 'publishedAt', 'slug'],
+  sort: 'publishedAt:desc',
+  pagination: {
+    pageSize: 4,
+    page: 1
+  },
+  populate: {
+    mainImage: true,
+    categories: {
+      fields: ['name']
     },
-    populate: {
-      mainImage: true,
-      createdBy: true,
-      categories: {
-        fields: ['name']
-      },
-      blogIcon: {
-        fields: ['url']
-      }
+    blogIcon: {
+      fields: ['url']
     }
-  }
-
-  try {
-    const { data } = await find<StrapiBlogs>('blogs', params)
-    if (data) {
-      latestBlogs.value = data.map((item) => item.attributes)
-    }
-  } catch (error) {
-    console.error(error)
   }
 }
-await fetchBlogs()
 
-
+const { data: latestBlogs } = await useAsyncData('latestBlog', () => find<StrapiBlogs>('blogs', params))
 </script>
 
 <style></style>
