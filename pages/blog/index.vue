ffe8aaaed698db475e174708920336a85fc7dc8b
Konkamon Sion <bigkonkamon@gmail.com>
2024-09-02 21:20:11 +0700

    feat(Blog): :sparkles: Using 'useAsyncData' / Better Error Display & Handling

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,31 +1,34 @@
 <template>
   <div>
-    <UAlert v-if="alertMessage" title="Error" :description="alertMessage"
-    color="red" variant="subtle" icon="ph:warning-circle-duotone" class="mb-5"
-    :close-button="{ icon: 'i-heroicons-x-mark-20-solid', color: 'red', variant: 'link', padded: false }"
-    @close="router.replace({ path: '/blog/' })"
-  />
-    <div class="flex flex-col lg:flex-row justify-between items-center mb-3">
+    <UAlert
+      v-if="alertMessage"
+      title="Error"
+      :description="alertMessage"
+      color="red"
+      variant="subtle"
+      icon="ph:warning-circle-duotone"
+      class="mb-5"
+      :close-button="{ icon: 'i-heroicons-x-mark-20-solid', color: 'red', variant: 'link', padded: false }"
+      @close="router.replace({ path: '/blog/' })"
+    />
+    <div class="mb-3 flex flex-col items-center justify-between lg:flex-row">
       <PageHeader title="My Blog" description="รวม Blog ต่างๆ ทั้งด้าน IT, Tips และอื่นๆ" />
-      <div class="mt-5 lg:m-0 ">
-        <span class="text-xs text-neutral-500">Powered by
-        </span>
-        <a href="https://strapi.io/" target="_blank" rel="noopener noreferrer"
-          class="ml-1 p-1 dark:rounded-lg dark:bg-indigo-100">
+      <div class="mt-5 lg:m-0">
+        <span class="text-xs text-neutral-500">Powered by </span>
+        <a href="https://strapi.io/" target="_blank" rel="noopener noreferrer" class="ml-1 p-1 dark:rounded-lg dark:bg-indigo-100">
           <UIcon name="logos:strapi" class="inline-block" mode="svg" />
         </a>
       </div>
     </div>
 
     <div class="my-4">
-      <UInput :loading="loading" type="text" size="lg" icon="ph:magnifying-glass" placeholder="ค้นหา Blog..."
-        v-model="searchInput" />
+      <UInput :loading="loading" type="text" size="lg" icon="ph:magnifying-glass" placeholder="ค้นหา Blog..." v-model="searchInput" />
     </div>
 
-    <div v-if="blogsData">
-      <section class="grid grid-cols-1 md:grid-cols-2 gap-3">
+    <main v-if="blogsData && blogsData.length > 0 && status === 'success'">
+      <section class="grid grid-cols-1 gap-3 md:grid-cols-2">
         <ClientOnly>
-          <div v-for="(post, index) in blogsData" :key="index">
+          <div v-for="post in blogsData" :key="post.slug">
             <LazyBlogIndexCard :post="post" />
           </div>
           <template #fallback>
@@ -35,25 +38,50 @@
           </template>
         </ClientOnly>
       </section>
-    </div>
-    <div v-else>
-      <UAlert icon="ic:round-search-off" title="ไม่พบ Blogs" :description="`ไม่พบ Blogs จากคำค้นหา ${searchInput}`"
-        color="primary" variant="soft" />
-    </div>
+    </main>
+    <UAlert
+      v-else-if="status === 'pending'"
+      title="Loading"
+      description="กำลังค้นหา Blog กรุณารอสักครู่"
+      icon="ph:magnifying-glass-duotone"
+      color="primary"
+      variant="soft"
+    />
+
+    <UAlert
+      v-else-if="route.query.search && blogsData?.length === 0 && status === 'success'"
+      icon="ic:round-search-off"
+      title="ไม่พบ Blogs"
+      :description="`ไม่พบ Blogs จากคำค้นหา ${route.query.search}`"
+      color="orange"
+      variant="subtle"
+    />
+    <UAlert
+      v-else-if="error?.statusCode || status === 'error'"
+      title="Error"
+      description="เกิดข้อผิดพลาดในการโหลดข้อมูล กรุณาลองใหม่อีกครั้งในภายหลัง"
+      icon="ph:magnifying-glass-duotone"
+      color="red"
+      variant="subtle"
+    />
   </div>
 </template>
 
 <script lang="ts" setup>
-import { type StrapiBlogs } from '~/types/StrapiBlogs';
+import { type StrapiBlogs } from '~/types/StrapiBlogs'
 
 const { find } = useStrapi()
 const loading = ref(false)
 const route = useRoute()
 const router = useRouter()
-const blogsData = ref<StrapiBlogs[]>()
-const searchInput = ref(route.query.search as string)
-
-const fetchBlogs = async (searchQuery?: string[]) => {
+const searchInput = ref('')
+const toast = useToast()
+const {
+  data: blogsData,
+  status,
+  error,
+  refresh
+} = await useAsyncData('blogs', async () => {
   const baseParams = {
     fields: ['title', 'subtitle', 'publishedAt', 'slug'],
     sort: 'publishedAt:desc',
@@ -67,29 +95,34 @@ const fetchBlogs = async (searchQuery?: string[]) => {
     }
   }
 
-  const searchParams = searchQuery
+  const searchParams = searchInput.value
     ? {
-      filters: {
-        $or: [{ title: { $containsi: searchQuery } }, { subtitle: { $containsi: searchQuery } }]
+        filters: {
+          $or: [{ title: { $containsi: searchInput.value } }, { subtitle: { $containsi: searchInput.value } }]
+        }
       }
-    }
     : {}
 
   const params = { ...baseParams, ...searchParams }
 
   try {
     const { data } = await find<StrapiBlogs>('blogs', params)
-
-    if (data) {
-      blogsData.value = data.map((item) => item.attributes)
-    }
+    return data ? data.map((item) => item.attributes) : []
   } catch (error) {
     console.error(error)
+    status.value = 'error'
+    return []
   }
+})
+
+if (error.value) {
+  toast.add({
+    title: 'Error',
+    description: 'เกิดข้อผิดพลาดในการโหลดข้อมูล กรุณาลองใหม่อีกครั้งในภายหลัง',
+    color: 'red',
+    icon: 'ph:warning-circle-duotone'
+  })
 }
-
-await fetchBlogs()
-
 let timeout: NodeJS.Timeout | null = null
 watch(searchInput, () => {
   if (timeout) {
@@ -97,12 +130,10 @@ watch(searchInput, () => {
   }
   timeout = setTimeout(async () => {
     await router.replace({ query: { search: searchInput.value } })
-    if (searchInput.value) {
-    } else {
+    if (!searchInput.value) {
       router.replace({ path: '/blog/' })
     }
-    await fetchBlogs([route.query.search as string, route.query.page as string])
-
+    await refresh()
   }, 500)
 })
 
@@ -124,5 +155,4 @@ const alertMessage = computed(() => {
   }
   return null
 })
-
 </script>
