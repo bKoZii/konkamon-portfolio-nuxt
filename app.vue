dc92ca1cae8d9cb938344b6b553e89d7b4de7f04
Konkamon Sion <bigkonkamon@gmail.com>
2024-09-05 10:21:18 +0700

    feat: :dizzy: Updated Page Transition
    Smoother Transition

--- app.vue
+++ app.vue
@@ -26,15 +26,17 @@ useHead({
 
 <style>
 @media (min-width: 400px) and (prefers-reduced-motion: no-preference) {
-  .page-enter-active,
+  .page-enter-active {
+    transition: all 0.5s cubic-bezier(0.25, 1, 0.25, 1);
+  }
   .page-leave-active {
-    transition: all 0.3s;
+    transition: all 0.3s cubic-bezier(0.75, 0, 0.75, 0);
   }
   .page-enter-from,
   .page-leave-to {
     opacity: 0;
     filter: blur(4px);
-    transform: translateY(20px);
+    transform: translateY(-20px);
   }
   .layout-enter-active,
   .layout-leave-active {
