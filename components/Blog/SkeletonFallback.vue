5a5bf0cf612c972b7f08c3553e56da090a006226
Konkamon Sion <bigkonkamon@gmail.com>
2024-09-09 14:37:25 +0700

    feat(Blog Fallback): :lipstick: Updated fallback to match current style

--- components/Blog/SkeletonFallback.vue
+++ components/Blog/SkeletonFallback.vue
@@ -1,10 +1,17 @@
 <template>
   <UCard class="h-fit">
-    <div class="flex flex-col space-y-4">
-      <USkeleton class="h-40 w-full" />
-      <USkeleton class="h-4 w-full" />
-      <USkeleton class="h-10 w-full" />
-      <USkeleton class="h-6 w-full" />
+    <div class="flex items-center w-full flex-row gap-5">
+      <USkeleton class="size-20" />
+      <div class="flex flex-col gap-2 items-start w-full">
+        <USkeleton class="h-5 w-full" />
+        <USkeleton class="h-4 w-full" />
+        <USkeleton class="h-4 w-1/3" />
+        <section class="flex flex-row gap-2">
+          <USkeleton class="w-10 h-5"/>
+          <USkeleton class="w-10 h-5"/>
+        </section>
+        <USkeleton class="h-6 w-20" />
+      </div>
     </div>
   </UCard>
 </template>
