b53b5690ec30d09a8fef09f5ca749fea9841b0b9
Konkamon Sion <bigkonkamon@gmail.com>
2024-09-19 11:44:33 +0700

    feat(Blog Card): :lipstick: Improved Text Size / Format / No Longer use Prose

--- components/Blog/IndexCard.vue
+++ components/Blog/IndexCard.vue
@@ -1,34 +1,39 @@
 <template>
   <template v-if="post">
     <NuxtLink :to="`/blog/${post.slug}`" prefetch-on="interaction">
-
-      <UCard :ui="{
-        base: 'transition-shadow relative h-full flex items-center',
-        ring: 'dark:ring-1 dark:hover:ring-2 dark:hover:ring-ocean-green-700',
-        background: 'dark:bg-neutral-900',
-        shadow: 'shadow hover:shadow-lg dark:shadow-none dark:hover:shadow-none',
-        body: {
-          base: 'hover:text-primary prose-h2:hover:text-primary',
-          padding: 'p-0 sm:px-0 sm:py-0 md:p-0'
-        }
-      }">
-        <div class="flex flex-row items-center p-4 gap-6">
-          <NuxtImg v-if="post.blogIcon" :src="`${post.blogIcon.data.attributes.url}`" width="100%"
-            class="w-12 sm:w-16 drop-shadow" :alt="`ไอคอนประจำโพสต์ ${post.title}`"
-            :aria-label="`ไอคอนประจำโพสต์ ${post.title}`" />
-          <div
-            class="md:prose-md prose prose-sm sm:prose-sm dark:prose-invert prose-h3:m-0 prose-p:m-0 flex max-w-none flex-col gap-2">
-            <h3 class="">{{ post.title }}</h3>
-            <p class="text-xs">{{ post.subtitle }}</p>
-            <section class="not-prose flex flex-row gap-1" v-if="post.categories">
+      <UCard
+        :ui="{
+          base: 'transition-shadow relative h-full flex items-center',
+          ring: 'dark:ring-1 dark:hover:ring-2 dark:hover:ring-ocean-green-700',
+          background: 'dark:bg-neutral-900',
+          shadow: 'shadow hover:shadow-lg dark:shadow-none dark:hover:shadow-none',
+          body: {
+            base: 'hover:text-primary prose-h2:hover:text-primary',
+            padding: 'p-0 sm:px-0 sm:py-0 md:p-0'
+          }
+        }"
+      >
+        <div class="flex flex-row items-center gap-4 p-4 md:gap-6">
+          <NuxtImg
+            v-if="post.blogIcon"
+            :src="`${post.blogIcon.data.attributes.url}`"
+            width="100%"
+            class="w-10 drop-shadow sm:w-16"
+            :alt="`ไอคอนประจำโพสต์ ${post.title}`"
+            :aria-label="`ไอคอนประจำโพสต์ ${post.title}`"
+          />
+          <div>
+            <h3 class="text-base font-semibold md:text-lg">{{ post.title }}</h3>
+            <p class="mt-1 text-xs text-neutral-600 dark:text-neutral-300">{{ post.subtitle }}</p>
+            <section class="my-2 flex flex-row gap-1" v-if="post.categories">
               <div v-for="category in post.categories.data" :key="category.id">
                 <UBadge size="xs" color="primary" variant="soft">{{ category.attributes.name }}</UBadge>
               </div>
             </section>
-            <div class="not-prose flex flex-row items-center gap-1">
-              <UIcon name="ph:calendar-dots-duotone" class="size-5" />
+            <div class="flex flex-row items-center gap-1">
+              <UIcon name="ph:calendar-dots-duotone" class="size-5 text-neutral-600 dark:text-neutral-200" />
               <UTooltip :text="useFormatDate(post.publishedAt)" :popper="{ arrow: true, strategy: 'absolute' }">
-                <p class="m-0 text-xs">{{ `${useRelativeDate(post.publishedAt)}` }}</p>
+                <span class="text-xs">{{ `${useRelativeDate(post.publishedAt)}` }}</span>
               </UTooltip>
             </div>
           </div>
