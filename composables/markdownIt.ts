aaca0eb21ab152bc41e3f30938be23f352c4b96a
Konkamon Sion <bigkonkamon@gmail.com>
2024-09-21 19:26:16 +0700

    feat(markdown-it): :sparkles: Use composable instead of plugin

--- composables/markdownIt.ts
+++ composables/markdownIt.ts
@@ -5,13 +5,11 @@ import MarkdownItGitHubAlerts from 'markdown-it-github-alerts'
 import 'markdown-it-github-alerts/styles/github-colors-light.css'
 import 'markdown-it-github-alerts/styles/github-colors-dark-class.css'
 import 'markdown-it-github-alerts/styles/github-base.css'
-export default defineNuxtPlugin(async () => {
-  const md = MarkdownIt({ linkify: true, typographer: true })
-  md.use(fromHighlighter(await ShikiHighlight, { theme: 'aurora-x' }))
-  md.use(MarkdownItGitHubAlerts)
-  return {
-    provide: {
-      markdownIt: md
-    }
-  }
-})
+
+const md = MarkdownIt({ linkify: true, typographer: true, html: false })
+md.use(fromHighlighter(await ShikiHighlight, { theme: 'aurora-x' }))
+md.use(MarkdownItGitHubAlerts)
+
+export const useMarkdownIt = (content: string) => {
+  return md.render(content)
+}
