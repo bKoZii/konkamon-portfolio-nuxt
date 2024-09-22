508009159102f9b3550546c796a8fc996f56dbe7
Konkamon Sion <bigkonkamon@gmail.com>
2024-09-22 10:58:05 +0700

    fix(markdownIt): :fire: Remove Unnecessary 'await'

--- composables/markdownIt.ts
+++ composables/markdownIt.ts
@@ -7,7 +7,7 @@ import 'markdown-it-github-alerts/styles/github-colors-dark-class.css'
 import 'markdown-it-github-alerts/styles/github-base.css'
 
 const md = MarkdownIt({ linkify: true, typographer: true, html: false })
-md.use(fromHighlighter(await ShikiHighlight, { theme: 'aurora-x' }))
+md.use(fromHighlighter(ShikiHighlight, { theme: 'aurora-x' }))
 md.use(MarkdownItGitHubAlerts)
 
 export const useMarkdownIt = (content: string) => {
