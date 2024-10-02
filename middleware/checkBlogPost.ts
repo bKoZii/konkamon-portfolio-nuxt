71571ad1c8025abb34bb39f07aa0a52a3a2e3eda
Konkamon Sion <bigkonkamon@gmail.com>
2024-10-02 14:15:57 +0700

    feat(Blog Middleware): :sparkles: Improved Code / ESLint Format

--- middleware/checkBlogPost.ts
+++ middleware/checkBlogPost.ts
@@ -1,34 +1,35 @@
 import type { StrapiBlogSlug } from '~/types/StrapiBlogSlug'
 
-export default defineNuxtRouteMiddleware(async (to, from) => {
+export default defineNuxtRouteMiddleware(async (to) => {
   const { findOne } = useStrapi()
   const slug = to.params.slug as string
   const slugCacheStore = useMySlugCacheStore()
 
   const cachedSlug = slugCacheStore.getSlug(slug)
 
-  try {
-    if (cachedSlug) {
-      if (cachedSlug !== slug) {
-        return navigateTo('/blog/')
-      }
-      return true
+  if (cachedSlug) {
+    if (cachedSlug !== slug) {
+      return navigateTo('/blog/')
     }
-    const { data } = await findOne<StrapiBlogSlug>('blogs', slug, {
-      fields: ['slug', 'publishedAt']
-    })
+    return true
+  }
 
-    if (data?.attributes.slug) {
-      if (data.attributes.publishedAt == null) {
-        return createError({ statusCode: 404, message: 'Blog ที่คุณกำลังเข้าถึง ได้ยกเลิกการเผยแพร่แล้ว' })
-      }
-      slugCacheStore.setSlug(slug, data.attributes.slug)
-    }
+  try {
+    const { data } = await findOne<StrapiBlogSlug>('blogs', slug, {
+      fields: ['slug', 'publishedAt'],
+    })
 
     if (!data?.attributes.slug || data.attributes.slug !== slug) {
       return navigateTo('/blog/')
     }
-  } catch (error) {
+
+    if (data.attributes.publishedAt == null) {
+      return createError({ statusCode: 404, message: 'Blog ที่คุณกำลังเข้าถึง ได้ยกเลิกการเผยแพร่แล้ว' })
+    }
+
+    slugCacheStore.setSlug(slug, data.attributes.slug)
+    return true
+  } catch {
     return createError({ statusCode: 404, message: 'ไม่พบ Blog ที่คุณกำลังเข้าถึง' })
   }
 })
