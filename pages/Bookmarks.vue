25c8a5e70950e5e1c80eb29c507353e66680c49a
Konkamon Sion <bigkonkamon@gmail.com>
2024-10-03 17:19:06 +0700

    feat(Bookmark): :sparkles: Sorting, 10 Items per Page

--- pages/Bookmarks.vue
+++ pages/Bookmarks.vue
@@ -11,7 +11,15 @@
         </template>
       </ClientOnly>
     </section>
-    <section v-if="bookmarkItems?.meta" class="mt-5 flex justify-center">
+    <section v-if="bookmarkItems?.meta" class="mt-5 flex justify-center gap-5">
+      <USelectMenu
+        v-model="sortBy"
+        :options="sortType"
+      >
+        <template #leading>
+          <UIcon v-if="sortBy.icon" :name="sortBy.icon" />
+        </template>
+      </USelectMenu>
       <UPagination
         v-model="currentPage"
         :page-count="itemPerPage"
@@ -24,15 +32,36 @@
 <script lang="ts" setup>
 import type BookmarkItemsInterface from '~/types/BookmarkItemsInterface'
 
+const sortType = [{
+  id: 'name:asc',
+  label: 'Name (Asc)',
+  icon: 'mdi:sort-alphabetical-ascending',
+}, {
+  id: 'name:desc',
+  label: 'Name (Desc)',
+  icon: 'mdi:sort-alphabetical-descending',
+}, {
+  id: 'createdAt:asc',
+  label: 'Added Date (Asc)',
+  icon: 'mdi:sort-clock-ascending-outline',
+}, {
+  id: 'createdAt:desc',
+  label: 'Added Date (Desc)',
+  icon: 'mdi:sort-clock-descending-outline',
+}]
+
+const sortBy = ref(sortType[0])
+
 const currentPage = ref(1)
-const itemPerPage = 6
+const itemPerPage = 10
 const nuxt = useNuxtApp()
 const { find } = useStrapi()
 const { data: bookmarkItems } = await useLazyAsyncData(
   'bookmarks',
-  () => find<BookmarkItemsInterface>('bookmarks', { pagination: { page: currentPage.value, pageSize: itemPerPage } }),
+  () => find<BookmarkItemsInterface>('bookmarks', { pagination: { page: currentPage.value, pageSize: itemPerPage }, sort: sortBy.value.id }),
   {
-    watch: [currentPage],
+    watch: [currentPage, sortBy],
+    deep: false,
     getCachedData(key) {
       if (nuxt.isHydrating && nuxt.payload.data[key]) {
         return nuxt.payload.data[key]
