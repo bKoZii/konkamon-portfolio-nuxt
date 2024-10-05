96650f9db69731c2a35d13586361c2a16b9a1320
Konkamon Sion <bigkonkamon@gmail.com>
2024-10-05 11:22:53 +0700

    perf(MDC API): :zap: Implement API Response Caching with 'lru-cache'
    Caching the parsed markdown response to prevent it parse the same content, and using hash key to keep the cache instead of entire parsed content.

--- server/api/mdc.post.ts
+++ server/api/mdc.post.ts
@@ -1,7 +1,33 @@
+import crypto from 'crypto'
 import { parseMarkdown } from '@nuxtjs/mdc/runtime'
+import { LRUCache } from 'lru-cache'
+import type { MDCRoot } from '@nuxtjs/mdc'
+
+function hashContent(content: string): string {
+  return crypto.createHash('sha256').update(content).digest('hex')
+}
+
+const cache = new LRUCache<string, MDCRoot>({
+  max: 100,
+  ttl: 1000 * 60 * 60 * 24 * 7, // 1 week
+})
 
 export default defineEventHandler(async (event) => {
-  const body = await readBody(event)
-  const ast = await parseMarkdown(body.content)
-  return ast.body
+  try {
+    const body = await readBody(event)
+    const cacheKey = hashContent(body.content)
+
+    if (cache.has(cacheKey)) {
+      return cache.get(cacheKey)
+    }
+
+    const ast = await parseMarkdown(body.content)
+
+    cache.set(cacheKey, ast.body)
+
+    return ast.body
+  } catch (error) {
+    console.error('Error processing request:', error)
+    throw createError({ statusCode: 500, statusMessage: 'Internal Server Error' })
+  }
 })
