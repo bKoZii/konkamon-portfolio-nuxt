bfa10be1a85ad35973254a559314c205a0cecbee
Konkamon Sion <bigkonkamon@gmail.com>
2024-10-05 14:36:26 +0700

    feat(MDC Component): :sparkles: Added Info Component

---
+++ components/global/Info.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <UAlert
+      color="primary"
+      title="ข้อควรรู้"
+      variant="subtle"
+      icon="ph:info-duotone"
+      :ui="{ title: 'font-bold', wrapper: 'not-prose', description: 'leading-6' }"
+    >
+      <template #description>
+        <MDCSlot unwrap="p" />
+      </template>
+    </UAlert>
+  </div>
+</template>
