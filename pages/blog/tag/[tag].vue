0ff9efbac192a1132835356d922cd63de218ff4f
Konkamon Sion <bigkonkamon@gmail.com>
2024-10-06 14:38:29 +0700

    feat(Blog Tag): :sparkles: Using Vueuse's useTimeoutFn / ESLint Format

--- pages/blog/tag/[tag].vue
+++ pages/blog/tag/[tag].vue
@@ -1,18 +1,24 @@
 <template>
   <div>
     <section class="flex flex-col items-center gap-2 md:flex-row">
-      <h1 class="text-3xl font-bold">All Blogs With Tag:</h1>
-      <UBadge size="lg" variant="subtle" :label="tagName" />
+      <h1 class="text-3xl font-bold">
+        All Blogs With Tag:
+      </h1>
+      <UBadge
+        size="lg"
+        variant="subtle"
+        :label="tagName"
+      />
     </section>
-    <section class="my-4" v-if="tagBlogs?.meta.pagination.total">
+    <section class="my-4">
       <UInput
+        id="searchInput"
+        v-model="searchInput"
         :loading="loading"
         type="text"
         size="lg"
         icon="ph:magnifying-glass"
         :placeholder="`ค้นหา Blog ที่มีแท็ก ${tagName}...`"
-        v-model="searchInput"
-        id="searchInput"
       >
         <template #trailing>
           <UKbd>F</UKbd>
@@ -38,7 +44,7 @@
         />
       </div>
     </section>
-    <section class="mt-5 flex justify-center" v-if="tagBlogs.meta.pagination.total">
+    <section v-if="tagBlogs?.meta.pagination.total" class="mt-5 flex justify-center">
       <UPagination
         v-model="currentPage"
         :total="tagBlogs.meta.pagination.total"
@@ -55,6 +61,7 @@
 <script lang="ts" setup>
 import type { RouteLocationNormalized } from 'vue-router'
 import type { StrapiBlogs } from '~/types/StrapiBlogs'
+
 const { find } = useStrapi()
 const route: RouteLocationNormalized = useRoute()
 const tagName = route.params.tag as string
@@ -70,18 +77,18 @@ const constructSearchFilters = (searchInput: string) => {
       {
         categories: {
           name: {
-            $containsi: tagName
-          }
-        }
-      }
+            $containsi: tagName,
+          },
+        },
+      },
     ],
     $and: [
       {
         title: {
-          $containsi: keyword
-        }
-      }
-    ]
+          $containsi: keyword,
+        },
+      },
+    ],
   }))
   return { $and: filters }
 }
@@ -96,16 +103,16 @@ const { refresh } = await useAsyncData(
       sort: 'publishedAt:desc',
       populate: {
         categories: {
-          fields: ['name']
+          fields: ['name'],
         },
         blogIcon: {
-          fields: ['url']
-        }
+          fields: ['url'],
+        },
       },
       pagination: {
         page: currentPage.value,
-        pageSize: pageSize
-      }
+        pageSize: pageSize,
+      },
     }),
   {
     deep: false,
@@ -118,10 +125,7 @@ const { refresh } = await useAsyncData(
       }
       return null
     },
-    default() {
-      return tagBlogs.value
-    }
-  }
+  },
 )
 
 useSeoMeta({
@@ -131,26 +135,29 @@ useSeoMeta({
   description: `รวม Blogs ที่มี Tag: '${tagName}'`,
   ogDescription: `รวม Blogs ที่มี Tag: '${tagName}'`,
   ogImage: '/ogImage-blogs.webp',
-  ogUrl: `https://www.konkamon.live/blog/tag/${tagName}`
+  ogUrl: `https://www.konkamon.live/blog/tag/${tagName}`,
 })
-let timeout: NodeJS.Timeout | null = null
+
+const { start } = useTimeoutFn(async () => {
+  loading.value = true
+  currentPage.value = 1
+  await refresh()
+  loading.value = false
+}, 500, { immediate: false })
+
 watch(searchInput, () => {
-  if (timeout) {
-    clearTimeout(timeout)
+  if (searchInput.value.length >= 3 && searchInput.value !== '') {
+    start()
+  } else if (searchInput.value === '') {
+    refresh()
   }
-  timeout = setTimeout(async () => {
-    loading.value = true
-    currentPage.value = 1
-    await refresh()
-    loading.value = false
-  }, 500)
 })
 
 defineShortcuts({
   f: {
     handler: () => {
       document.getElementById('searchInput')?.focus()
-    }
-  }
+    },
+  },
 })
 </script>
