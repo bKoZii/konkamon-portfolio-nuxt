2c583a0f28a99df6c47c0eacd2b9072c05ce983e
Konkamon Sion <bigkonkamon@gmail.com>
2024-10-18 14:28:21 +0700

    fix(MDC Component): :lipstick: ProseCode: Dark mode style

--- components/global/ProseCode.vue
+++ components/global/ProseCode.vue
@@ -1,5 +1,5 @@
 <template>
   <span class="before:prose-code:content-none after:prose-code:content-none">
-    <code class="rounded border border-neutral-300 bg-slate-200 px-1 font-normal"><slot /></code>
+    <code class="rounded border border-neutral-300 bg-slate-200 px-1 font-normal dark:border-neutral-700 dark:bg-neutral-800"><slot /></code>
   </span>
 </template>
