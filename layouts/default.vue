cb175893ba71f0e538d53bcddbc966399c310290
Konkamon Sion <bigkonkamon@gmail.com>
2024-10-19 15:23:52 +0700

    feat(app): :dizzy: Added Particle Background

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
+    <div class="fixed place-items-center w-full min-h-screen -z-10">
+      <div class="relative h-[400px]">
+        <Particles class="absolute top-0" color="#379777" />
+        <ParticleBackground color="#379777" />
+      </div>
+    </div>
     <MainNavbar />
     <UContainer class="flex max-w-[1000px] flex-col flex-nowrap gap-2 sm:gap-5" :ui="{ padding: 'p-0 xs:p-0 sm:p-3' }">
       <main class="px-3 my-5  md:p-0">
