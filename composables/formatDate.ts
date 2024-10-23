72fa35570167c5e01d8a56eafcf8772df9ddfa4a
Konkamon Sion <bigkonkamon@gmail.com>
2024-10-23 13:07:22 +0700

    feat(day.js): :sparkles: Now watch for a locale change.

--- composables/formatDate.ts
+++ composables/formatDate.ts
@@ -1,4 +1,7 @@
 const dayjs = useDayjs()
+
 export const useFormatDate = (date: string) => {
+  const { locale } = useI18n()
+  dayjs.locale(locale.value)
   return dayjs(date).format('D MMMM YYYY - HH:mm')
 }
--- composables/relativeDate.ts
+++ composables/relativeDate.ts
@@ -1,4 +1,7 @@
 const dayjs = useDayjs()
+
 export const useRelativeDate = (date: string) => {
+  const { locale } = useI18n()
+  dayjs.locale(locale.value)
   return dayjs().to(dayjs(date))
 }
